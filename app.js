d70b32bbf8c6f9619144ee6b19af8de506c4b8cc
Russ Perry <rperry1886@gmail.com>
2020-01-05 12:03:25 -0500

    Make the navbar disappear when scrolling down, and reappear if you scroll up

---
+++ app.js
@@ -0,0 +1,10 @@
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (this.prevScrollPos > currentScrollPos) {
+    this.document.getElementById("navbar").style.top = "0";
+  } else {
+    this.document.getElementById("navbar").style.top = "-70px";
+  }
+  this.prevScrollPos = currentScrollPos;
+};
