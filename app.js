f329e3ddeec615f388e3cff899fb203da1c5ba65
Russ Perry <rperry1886@gmail.com>
2020-05-17 21:03:32 -0400

    Remove borders
    Remove borders around testimonials and images

--- app.js
+++ app.js
@@ -18,3 +18,20 @@ window.onscroll = function() {
 document.getElementById("submit").addEventListener("click", event => {
   event.preventDefault();
 });
+
+for (let j = 0; j < str.length; j++) {
+  charNumber = str.charCodeAt(j);
+  if (charNumber === 32) {
+    newStr += " ";
+  } else {
+    for (let i = 0; i < 13; i++) {
+      if (charNumber < 90) {
+        charNumber++;
+      } else {
+        charNumber = 65;
+        charNumber++;
+      }
+    }
+  }
+}
+return str;
